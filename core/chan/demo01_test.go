1c69428e03646a7d2f1939445e229549a0aec359
nx4iZ2P7Qg <msP02n1725@protonmail.com>
2021-04-30 15:30:20 +0800

    struct{}{}

--- core/chan/demo01_test.go
+++ core/chan/demo01_test.go
@@ -23,3 +23,12 @@ func TestDemo0101(t *testing.T) {
 	fmt.Println("bbb")
 	ch <- struct{}{}
 }
+
+func TestDemo0102(t *testing.T) {
+	ch := make(chan struct{}, 1)
+	ch <- struct{}{}
+	// 输出 1，可见上一步的操作不仅仅是个信号，它要占用1个位置
+	println(len(ch))
+	// 继续添加会报错
+	//ch <- struct{}{}
+}
